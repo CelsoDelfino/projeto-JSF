773d798e2ae1e58f1aa72958ec2dbcfb76282ef1
Celso Delfino <celsinhodelfino@hotmail.com>
2024-01-09 14:10:26 -0300

    add formulario de cadastro

--- src/main/java/com/delfino/controller/GestaoEmpresasBean.java
+++ src/main/java/com/delfino/controller/GestaoEmpresasBean.java
@@ -8,26 +8,54 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.delfino.model.Empresa;
+import com.delfino.model.TipoEmpresa;
 import com.delfino.repository.Empresas;
+import com.delfino.util.FacesMessages;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    public GestaoEmpresasBean() {}
-    
-    @Inject
-    private Empresas empresas;
-    
-    private List<Empresa> listaEmpresas;
-    
-    public void todasEmpresas() {
-        listaEmpresas = empresas.todas();
-    }
-    
-    public List<Empresa> getListaEmpresas() {
-        return listaEmpresas;
-    }
+	private static final long serialVersionUID = 1L;
+
+	public GestaoEmpresasBean() {
+	}
+
+	@Inject
+	private Empresas empresas;
+	
+	@Inject
+	private FacesMessages messages;
+
+	private String termoPesquisa;
+	
+	public void pesquisar() {
+		listaEmpresas = empresas.pesquisar(termoPesquisa);
+		
+		if(listaEmpresas.isEmpty()) {
+			messages.info("Sua consulta não retornou registros.");
+		}
+	}
+
+	private List<Empresa> listaEmpresas;
+
+	public void todasEmpresas() {
+		listaEmpresas = empresas.todas();
+	}
+
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
+	}
+
+	public String getTermoPesquisa() {
+		return termoPesquisa;
+	}
+
+	public void setTermoPesquisa(String termoPesquisa) {
+		this.termoPesquisa = termoPesquisa;
+	}
+	
+	public TipoEmpresa[] getTiposEmpresa() {
+		return TipoEmpresa.values();
+	}
 }
